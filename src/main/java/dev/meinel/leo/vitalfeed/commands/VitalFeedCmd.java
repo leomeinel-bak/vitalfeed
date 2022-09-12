030dd65b9cecfc98608c7f2adaa5dcc45f5fe902
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-13 01:52:07 +0200

    Change indentation to spaces

--- src/main/java/dev/meinel/leo/vitalfeed/commands/VitalFeedCmd.java
+++ src/main/java/dev/meinel/leo/vitalfeed/commands/VitalFeedCmd.java
@@ -23,37 +23,37 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Map;
 
 public class VitalFeedCmd
-		implements CommandExecutor {
+        implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
-			@NotNull String[] args) {
-		if (Cmd.isArgsLengthGreaterThan(sender, args, 1)) {
-			return false;
-		}
-		doFeed(sender, args);
-		return true;
-	}
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+            @NotNull String[] args) {
+        if (Cmd.isArgsLengthGreaterThan(sender, args, 1)) {
+            return false;
+        }
+        doFeed(sender, args);
+        return true;
+    }
 
-	private void doFeed(@NotNull CommandSender sender, @NotNull String[] args) {
-		if (Cmd.isInvalidSender(sender)) {
-			return;
-		}
-		Player senderPlayer = (Player) sender;
-		if (args.length == 1) {
-			Player player = Bukkit.getPlayer(args[0]);
-			if (CmdSpec.isInvalidCmd(senderPlayer, player, "vitalfeed.feed.others")) {
-				return;
-			}
-			assert player != null;
-			Chat.sendMessage(sender, Map.of("%player%", player.getName()), "player-fed");
-			player.setFoodLevel(20);
-			return;
-		}
-		if (CmdSpec.isInvalidCmd(sender, "vitalfeed.feed")) {
-			return;
-		}
-		Chat.sendMessage(senderPlayer, "fed");
-		senderPlayer.setFoodLevel(20);
-	}
+    private void doFeed(@NotNull CommandSender sender, @NotNull String[] args) {
+        if (Cmd.isInvalidSender(sender)) {
+            return;
+        }
+        Player senderPlayer = (Player) sender;
+        if (args.length == 1) {
+            Player player = Bukkit.getPlayer(args[0]);
+            if (CmdSpec.isInvalidCmd(senderPlayer, player, "vitalfeed.feed.others")) {
+                return;
+            }
+            assert player != null;
+            Chat.sendMessage(sender, Map.of("%player%", player.getName()), "player-fed");
+            player.setFoodLevel(20);
+            return;
+        }
+        if (CmdSpec.isInvalidCmd(sender, "vitalfeed.feed")) {
+            return;
+        }
+        Chat.sendMessage(senderPlayer, "fed");
+        senderPlayer.setFoodLevel(20);
+    }
 }
